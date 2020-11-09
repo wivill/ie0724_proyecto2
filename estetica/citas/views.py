6891971b07beadb5e279486f320d80c94165315b
bernardo <bjza09@hotmail.com>
2020-11-08 23:19:23 -0600

    Video 2 listo

--- estetica/citas/views.py
+++ estetica/citas/views.py
@@ -1,10 +1,41 @@
 from django.shortcuts import render
 from django.http import HttpResponse, Http404
 from .models import usuario
+from .forms import userForm
 
 # Create your views here.
-def new(request):    
-    return HttpResponse('Showing \'new\' view page')
+def new(request):
+    new_form = userForm()
+
+    if request.method == 'POST':
+        filled_form = userForm(request.POST)
+        if filled_form.is_valid():
+            new_user = filled_form.save()
+            note = (
+                'Usuario con identificador \'{}\' fue creado exitosamente.\n'
+                'Nombre: {}'.format(
+                    new_user.pk, filled_form.cleaned_data['name']
+                )
+            )
+        else:
+            note = 'Formulario inválido'
+        return render(
+            request,
+            'new.html',
+            {
+                'personform': new_form,
+                'note': note,
+            }
+        )
+    else:
+        return render(
+            request,
+            'new.html',
+            {
+                'personform': new_form,
+                'note': 'Hola!'
+            }
+        )
 
 def show(request, pk=None):
     if pk is not None:
