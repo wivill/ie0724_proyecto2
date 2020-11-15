1204e9992b29bde1f7e1ab5f135fe9054cd7f9e4
wivill <willy.villalobos@ucr.ac.cr>
2020-11-14 20:24:17 -0600

    Password reset listo.

--- estetica/estetica/settings.py
+++ estetica/estetica/settings.py
@@ -136,3 +136,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 # LOGOUT_URL = '/'
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
+
+EMAIL_BACKEND = "django.core.mail.backends.filebased.EmailBackend"
+EMAIL_FILE_PATH = str(BASE_DIR.joinpath('sent_emails'))
