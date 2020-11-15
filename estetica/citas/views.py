1ead966ec4653c60cf545bc4084b85fcc4137568
wivill <willy.villalobos@ucr.ac.cr>
2020-11-14 20:37:11 -0600

    Detalles de formato.

--- estetica/citas/views.py
+++ estetica/citas/views.py
@@ -1,12 +1,12 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render
 # from django.http import HttpResponse, Http404
 from django.http import Http404
 from .models import usuario
 from .forms import userForm
-from .new_user_form import NewUserForm
-from django.contrib.auth import login, authenticate, logout
-from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
-from django.contrib import messages
+# from .new_user_form import NewUserForm
+from django.contrib.auth import login
+# from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+# from django.contrib import messages
 # from django.contrib.auth.decorators import login_required
 
 
@@ -75,58 +75,3 @@ def show(request, pk=None):
                 'users_dict': users_dict
             }
         )
-
-
-# def home(request):
-#     return render(
-#         request, 'home.html'
-#     )
-
-
-# def register_request(request):
-# 	if request.method == "POST":
-# 		form = NewUserForm(request.POST)
-# 		if form.is_valid():
-# 			user = form.save()
-# 			login(request, user)
-# 			messages.success(request, "Registro exitoso." )
-# 			return redirect("home.html")
-# 		messages.error(
-#             request,
-#             "Su registro ha fallado. Información inválida."
-#         )
-# 	form = NewUserForm
-# 	return render (
-#         request=request,
-#         template_name="signup.html",
-#         context={"register_form":form}
-#     )
- 
- 
-# def login_request(request):
-# 	if request.method == "POST":
-# 		form = AuthenticationForm(request, data=request.POST)
-# 		if form.is_valid():
-# 			username = form.cleaned_data.get('username')
-# 			password = form.cleaned_data.get('password')
-# 			user = authenticate(username=username, password=password)
-# 			if user is not None:
-# 				login(request, user)
-# 				messages.info(request, f"You are now logged in as {username}.")
-# 				return redirect("home.html")
-# 			else:
-# 				messages.error(request,"Invalid username or password.")
-# 		else:
-# 			messages.error(request,"Invalid username or password.")
-# 	form = AuthenticationForm()
-# 	return render(
-#         request=request,
-#         template_name="login.html",
-#         context={"login_form":form}
-#      )
-
-
-# def logout_request(request):
-# 	logout(request)
-# 	messages.info(request, "You have successfully logged out.") 
-# 	return redirect("home.html")
